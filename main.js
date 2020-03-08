9f0f597e4819552a003f5e0d835a0f51da11d481
dchhitarka <dchhitarka@gmail.com>
2020-03-08 23:58:05 +0530

    Almost ready

--- main.js
+++ main.js
@@ -1,50 +1,107 @@
+// Sticky navbar
+window.onscroll = function() {
+    stickyNav();
+    counterRun();
+};
+let navbar = document.getElementById("nav-menu");
+let sticky = navbar.offsetTop;
+function stickyNav() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("nav-sticky");;
+  } else {
+    navbar.classList.remove("nav-sticky");
+  }
+}
 // Menu Toggle
 let bar = document.querySelector(".nav-bars");
+let navList = document.querySelector(".nav-list");
 bar.addEventListener("click", ()=>{
-    let navList = document.querySelector(".nav-list");
-    if(navList.style.display == 'none'){
-        navList.style.display = "flex";
-        console.log(`click`);
-    }
+    console.log(navList.length);
+    navList.classList.toggle('nav-list-mobile');
 }, false)
+
+// Nav Item active
+let navItem = document.querySelectorAll(".nav-item");
+for (let i = 0; i < navItem.length; i++) {
+    navItem[i].addEventListener("click", function() {
+        let current = document.querySelector(".nav-item-active");
+        current.classList.remove("nav-item-active");
+        navItem[i].classList.add("nav-item-active");
+  });
+}
+// Top Margin for hero section
+let navHeight = navList.offsetHeight;
+let heroSection = document.querySelector('.hero-section');
+
 // Hero Slideshow
 let slideIndex = 1;
-showDivs(slideIndex);
-slideShow(); //Automatic SlideShow
+let dots = document.getElementsByClassName("slide-dots");
+let slides = document.getElementsByClassName("mySlides");
 
-function currentDiv(n) {
-  showDivs(slideIndex = n);
+for(let i = 0; i < dots.length; i++){
+    dots[i].addEventListener("click", ()=>{
+        showDivs(i+1, dots, slides);
+    }, false);
+}
+showDivs(slideIndex, dots, slides);
+slideShow(dots, slides); //Automatic SlideShow
+
+// Testimonial Slideshow
+let testimonial_dots = document.getElementsByClassName("dots");
+let testimonial_slides = document.getElementsByClassName("testimonial");
+
+for(let i = 0; i < testimonial_dots.length; i++){
+    testimonial_dots[i].addEventListener("click", ()=>{
+        showDivs(i+1, testimonial_dots, testimonial_slides);
+    }, false);
+}
+showDivs(slideIndex-1, testimonial_dots, testimonial_slides);
+// slideShow(testimonial_dots, testimonial_slides); //Automatic SlideShow
+
+function showDivs(n, dots, x) {
+      slideIndex = n;
+      if (n > x.length) {slideIndex = 1}
+      if (n < 1) {slideIndex = x.length}
+      for (let i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+      }
+      for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace("active-dot", "");
+      }
+      x[slideIndex-1].style.display = "flex";
+      dots[slideIndex-1].className += " active-dot";
 }
 
-function showDivs(n) {
-  let i;
-  let x = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("slide-dots");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace("active-dot", "");
-  }
-  x[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active-dot";
-}
-
-function slideShow(){
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("slide-dots");
-    for (i = 0; i < slides.length; i++) {
+function slideShow(dots, slides){
+    for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    for(i = 0; i < dots.length; i++){
+    for(let i = 0; i < dots.length; i++){
         dots[i].classList.remove('active-dot');
     }
-    slideIndex++;
     if (slideIndex > slides.length) {slideIndex = 1}
     slides[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " active-dot";
-    setTimeout(slideShow, 2000);
+    slideIndex++;
+    setTimeout(()=>{
+        slideShow(dots, slides)
+    }, 3000);
+}
+
+// Counter
+import { CountUp } from './countUp.min.js';
+window.onload = counter();
+function counter() {
+    let count1 = new CountUp('count-software', 5, 1);
+    count1.start();
+    let count2 = new CountUp('count-website', 10, 1.5);
+    count2.start();
+    let count3 = new CountUp('count-app', 15, 2);
+    count3.start();
+};
+function counterRun(){
+    let counterSec = document.querySelector('.sec-counter');
+    if(window.pageYOffset > counterSec.offsetHeight){
+        counter();
+    }
 }
